77af20f73f0730f70571e55dbe4a219e79aefa25
Alena Zhmurko <alenazhmurko@gmail.com>
2021-10-01 22:28:27 +0300

    codereview fixes

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-let title = "Repo task";
-let screens = "Simple, Complex, Interactive";
-let screenPrice = 25;
-let rollback = 97;
-let fullPrice = 5000000;
-let adaptive = true;
+const title = "Repo task";
+const screens = "Simple, Complex, Interactive";
+const screenPrice = 25;
+const rollback = 97;
+const fullPrice = 5000000;
+const adaptive = true;
 
-console.log("title: "+ typeof(title) +"\nfullPrice: "+ typeof(fullPrice) + "\nadaptive: "+ typeof(adaptive));
+console.log(
+  "title: " +
+    typeof title +
+    "\nfullPrice: " +
+    typeof fullPrice +
+    "\nadaptive: " +
+    typeof adaptive
+);
 console.log(screens.length);
-console.log(`Стоимость верстки экранов ${screenPrice} рублей/ долларов/гривен/юани`);
-console.log(`Стоимость разработки сайта ${fullPrice} рублей/ долларов/гривен/юани`);
-console.log(screens.toLowerCase().split(', '))
-console.log(fullPrice * (rollback/100))
+console.log(
+  `Стоимость верстки экранов ${screenPrice} гривен\nСтоимость разработки сайта ${fullPrice} гривен`
+);
+console.log(screens.toLowerCase().split(", "));
+console.log(fullPrice * (rollback / 100));
