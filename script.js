35d51e613ca595fe15e5606a6225defde97d42b4
Alena Zhmurko <alenazhmurko@gmail.com>
2021-10-01 16:35:30 +0300

    2nd task

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = "Repo task";
+let screens = "Simple, Complex, Interactive";
+let screenPrice = 25;
+let rollback = 97;
+let fullPrice = 5000000;
+let adaptive = true;
 
-alert("Hello!");
-console.log("Hi from console!");
+console.log("title: "+ typeof(title) +"\nfullPrice: "+ typeof(fullPrice) + "\nadaptive: "+ typeof(adaptive));
+console.log(screens.length);
+console.log(`Стоимость верстки экранов ${screenPrice} рублей/ долларов/гривен/юани`);
+console.log(`Стоимость разработки сайта ${fullPrice} рублей/ долларов/гривен/юани`);
+console.log(screens.toLowerCase().split(', '))
+console.log(fullPrice * (rollback/100))
